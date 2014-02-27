f3a55a45ca94a1270e8135c35c4463415bcd9b60
Zederich <zederich0@gmail.com>
2014-02-27 21:42:25 +0100

    added support for settings file
    Now all the setting for this program can be controlled via the files
    settings.txt in the user's appdata\roaming folder. The refresh rate and
    image URL can be set. I did not use f.readline(), because this was very
    slow.

--- src/main.py
+++ src/main.py
@@ -6,10 +6,23 @@ from os import environ
 
 pathtoimg = environ['TEMP']+"\\bg.jpg"  #The file bg.jpg in the temp folder
 
+print(pathtoimg)
+
+settings = open(environ['APPDATA']+"\\livewebimagedesktop\\settings.txt")  #Settings file
+
+lines = []
+count = 0
+for line in settings:
+    count += 1  
+    lines.append(line)
+
+imgurl = lines[0]
+delay = float(lines[1])
+print(delay)
 
 def get_image():
     f = open(pathtoimg, 'wb')           #Open/Create image
-    f.write(request.urlopen('http://lorempixel.com/sports/1900/1080/').read())  #Get web image and overwrite
+    f.write(request.urlopen(imgurl).read())  #Get web image and overwrite
     f.close()
 
 while 1: #Forever
@@ -18,4 +31,4 @@ while 1: #Forever
           pythoncom.CLSCTX_INPROC_SERVER, shell.IID_IActiveDesktop)
     iad.SetWallpaper(pathtoimg, 0)  #Set wallpaper
     iad.ApplyChanges(shellcon.AD_APPLY_ALL)
-    sleep(60) #Wait (seconds)
+    sleep(delay) #Wait (seconds)
