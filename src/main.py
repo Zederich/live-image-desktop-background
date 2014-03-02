11e7adbc661ef25ce4ecb85f663a76fa81fb6e5d
Zederich <zederich0@gmail.com>
2014-03-02 18:02:24 +0100

    Added autodetection if file is updated
    If the thired line of the settings.txt file is set to "True" or "true",
    etc. the program will ignore the set delay, but update every time it
    notices a change in the image file size (what are the odds of 2 large
    photos having exactly the same number of bytes?).  At the moment it is
    set to look for changes in file size every 4 seconds.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 import pythoncom
-from urllib import request
+from urllib.request import urlopen
 from win32com.shell import shell, shellcon
 from time import sleep
 from os import environ
+import tkinter.messagebox as messagebox
 
 pathtoimg = environ['TEMP']+"\\bg.jpg"  #The file bg.jpg in the temp folder
 
@@ -10,19 +11,58 @@ settings = open(environ['APPDATA']+"\\livewebimagedesktop\\settings.txt",'r')  #
 
 lines = []
 count = 0
-for line in settings:
-    count += 1  
+for line in settings: 
     lines.append(line)
 
 imgurl = lines[0]
 delay = float(lines[1])
+print(lines[2].lower() == 'true')
+should_autodetect = lines[2].lower() == 'true'
+print(should_autodetect)
+print(type(should_autodetect))
 
-while 1: #Forever
-    img = open(pathtoimg, 'wb')           #Open/Create image
-    img.write(request.urlopen(imgurl).read())  #Get web image and overwrite
-    img.close()
-    iad = pythoncom.CoCreateInstance(shell.CLSID_ActiveDesktop, None,
-          pythoncom.CLSCTX_INPROC_SERVER, shell.IID_IActiveDesktop)
-    iad.SetWallpaper(pathtoimg, 0)  #Set wallpaper
-    iad.ApplyChanges(shellcon.AD_APPLY_ALL)
-    sleep(delay) #Wait (seconds)
+
+def getfilesize():
+    opened = urlopen(imgurl)
+    if "Content-Length" in opened.headers:
+        size = int(opened.headers["Content-Length"])
+    else:
+        size = len (opened.read ());
+        print("dl")
+    return size
+
+
+if should_autodetect:
+    print("Auto")
+    fsize = getfilesize()
+    initialised = True
+    while 1: #Forever
+        if getfilesize() != fsize or initialised: #Make sure it enters the if for the first time.
+            print("Updated")
+            print(getfilesize())
+            img = open(pathtoimg, 'wb')           #Open/Create image
+            img.write(urlopen(imgurl).read())  #Get web image and overwrite
+            img.close()
+            iad = pythoncom.CoCreateInstance(shell.CLSID_ActiveDesktop, None,
+                  pythoncom.CLSCTX_INPROC_SERVER, shell.IID_IActiveDesktop)
+            iad.SetWallpaper(pathtoimg, 0)  #Set wallpaper
+            iad.ApplyChanges(shellcon.AD_APPLY_ALL)
+            fsize = getfilesize()
+        else:
+            pass
+        sleep(5)
+        if initialised: #Prevent the variable being changed every cycle.
+            initialised = False
+        else:
+            pass
+
+else:
+    while 1: #Forever
+        img = open(pathtoimg, 'wb')           #Open/Create image
+        img.write(urlopen(imgurl).read())  #Get web image and overwrite
+        img.close()
+        iad = pythoncom.CoCreateInstance(shell.CLSID_ActiveDesktop, None,
+              pythoncom.CLSCTX_INPROC_SERVER, shell.IID_IActiveDesktop)
+        iad.SetWallpaper(pathtoimg, 0)  #Set wallpaper
+        iad.ApplyChanges(shellcon.AD_APPLY_ALL)
+        sleep(delay) #Wait (seconds)
