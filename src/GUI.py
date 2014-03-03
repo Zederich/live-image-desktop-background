49693ed1be9dc6116e7b1374bf61dbc239582840
Zederich <zederich0@gmail.com>
2014-03-03 13:27:05 +0100

    Added GUI option for autodetection of updates
    There is now a checkbox in the GUI. If it is clicked, in the
    settings.txt it will write "True", enabling autodetection in main.py.

--- src/GUI.py
+++ src/GUI.py
@@ -17,14 +17,21 @@ def to_orig():  #Will revert settings to how it was before program executed
     settings.close()
     messagebox.showinfo("Successfully Reset","The delay and time have been reset to:\n\n{0}".format(to_write))
 
+def toggle_delay():
+    if should_auto.get() == "True":
+        delay.configure(state="disabled")
+    else:
+        delay.configure(state="normal")
+
 def save_new(shouldquit):
+    print(should_auto.get())
     new_url = str(url.get()) #Get input for the URL.
     new_delay = delay.get()  #Get input for "delay"
     if "http://" in new_url and "." in new_url: #Some tests to make sure URL is valid.
         if new_delay.isnumeric():   #Check that the value for "delay" is a number
             if  1 <= float(new_delay) <= 36000:
                 settings = open("{0}\\livewebimagedesktop\\settings.txt".format(environ['APPDATA']),'w')
-                settings.write(new_url+"\n"+new_delay)  #Wite new input values to file
+                settings.write( "{0}\n{1}\n{2}".format(new_url,new_delay, should_auto.get()) )  #Wite new input values to file
                 settings.close()
                 messagebox.showinfo("Done!","your changes have been saved. You will now need to restart the Live-Web-Image Program for the changes to take effect.")
                 if shouldquit:  #Close window
@@ -46,22 +53,26 @@ def save_new_open(): #Save but keep window open
     save_new(False)
 
 
-Label(root, text="URL of image").grid(row=1, sticky=W, padx=7, pady=0)
-Label(root, text="Delay (secs)").grid(row=1, column=2, sticky=W, padx=7, pady=0)
+Label(root, text="URL of image").grid(row=1, padx=7, pady=0, columnspan=2)
+Label(root, text="Delay (secs)").grid(row=1, column=2, columnspan=2, padx=7, pady=0)
+
+should_auto = StringVar()
 
 url = Entry(root, width = 24, font=("Verdana", 13))
 delay = Spinbox(root, from_=5, to=3600, width=5, increment=5, font=("Verdana", 13))
+autodetect = Checkbutton(root, text="Autodetect", var=should_auto, onvalue = "True", offvalue = "False", command=toggle_delay)
 
 url.grid(row=0, column=0, padx=7, pady=5, columnspan=2, sticky=W)
-delay.grid(row=0, column=2, padx=7, pady=5, sticky=W)
+delay.grid(row=0, column=3, padx=7, pady=5, sticky=W)
+autodetect.grid(row=0, column=2, padx=7, pady=5, sticky=W)
 
 
 ok_btn = Button(text="OK!",width=20, command=save_new_cls).grid(row=2, column=0, sticky=SW, padx=6, pady=22, ipady=5)         #OK Button
 apply_btn = Button(text="Apply", width=20, command=save_new_open).grid(row=2, column=1, sticky=SW, padx=4, pady=22, ipady=5)  #Apply Button
-reset_btn = Button(text="Reset", width=11,command=to_orig).grid(row=2, column=2, sticky=SW, padx=6, pady=22, ipady=5)         #Reset Button
+reset_btn = Button(text="Reset", width=11,command=to_orig).grid(row=2, column=2, columnspan=2, sticky=SW, padx=6, pady=22, ipady=5)         #Reset Button
 
 
-root.geometry("374x125")  #Window size
+root.geometry("466x125")  #Window size
 root.resizable(0,0)
 
 root.mainloop()
